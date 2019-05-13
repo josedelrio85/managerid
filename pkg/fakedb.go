43b60c746d3c6ee3074da7e2d85c1f8d758b663d
Jose del Río <joserio@bysidecar.com>
2019-05-13 13:38:32 +0200

    Added Close and CreateTable functions with its configuration to support implementation of the Queriergorm interface.

--- pkg/fakedb.go
+++ pkg/fakedb.go
@@ -6,8 +6,12 @@ import "sync"
 type FakeDb struct {
 	OpenFunc           func() error
 	OpenCalls          int
-	CheckIdentityFunc  func(Interaction) (*Identity, error)
+	CheckIdentityFunc  func(Interaction) (*Identitygorm, error)
 	CheckIdentityCalls int
+	CloseFunc          func() error
+	CloseCalls         int
+	CreateTableFunc    func() error
+	CreateTableCalls   int
 
 	sync.Mutex
 }
@@ -21,9 +25,25 @@ func (f *FakeDb) Open() error {
 }
 
 // CheckIdentity is a method to test CheckIdentity function
-func (f *FakeDb) CheckIdentity(interaction Interaction) (*Identity, error) {
+func (f *FakeDb) CheckIdentity(interaction Interaction) (*Identitygorm, error) {
 	f.Lock()
 	defer f.Unlock()
 	f.CheckIdentityCalls++
-	return f.CheckIdentity(interaction)
+	return f.CheckIdentityFunc(interaction)
+}
+
+// Close is a method to test Close function
+func (f *FakeDb) Close() {
+	f.Lock()
+	defer f.Unlock()
+	f.CloseCalls++
+	f.CloseFunc()
+}
+
+// CreateTable is a method to test CreateTable function
+func (f *FakeDb) CreateTable() error {
+	f.Lock()
+	defer f.Unlock()
+	f.CreateTableCalls++
+	return f.CreateTableFunc()
 }
