a7c88becb43c3e6c9ee2ddc3324da719215902b0
Jose del Río <joserio@bysidecar.com>
2019-07-17 13:56:17 +0200

    Changed names of idgroup and id for PassportIDGrp and PassportID

--- sql_test/test.sql
+++ sql_test/test.sql
@@ -2,8 +2,8 @@ CREATE TABLE `identities_bsc` (
   `ip` varchar(45) CHARACTER SET utf8 COLLATE utf8_spanish_ci DEFAULT NULL,
   `provider` varchar(255) COLLATE utf8_spanish_ci DEFAULT NULL,
   `application` varchar(255) COLLATE utf8_spanish_ci DEFAULT NULL,
-  `idgroup` varchar(45) COLLATE utf8_spanish_ci DEFAULT NULL,
-  `id` varchar(45) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `passport_id_grp` varchar(45) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `passport_id` varchar(45) COLLATE utf8_spanish_ci DEFAULT NULL,
   `createdat` datetime DEFAULT NULL,
   `ididentity` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`ididentity`)
